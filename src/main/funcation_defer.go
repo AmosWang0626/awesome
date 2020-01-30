592be1ec88c036a7dfe30164db90991940ee42cc
amos.wang <1833063210@qq.com>
2020-01-30 11:07:47 +0800

    feat: defer 最佳实践

--- src/main/funcation_defer.go
+++ src/main/funcation_defer.go
@@ -3,16 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("555555555555 sum=", sum(100, 200))
-	fmt.Println("666666666666")
+	fmt.Println("444 sum =", sum(100, 200))
+	fmt.Println(".........end.............")
+
+	// monitor db run sql
+	monitor()
 }
 
 func sum(n1, n2 int) int {
-	defer fmt.Println("111111111111")
-	defer fmt.Println("222222222222")
-	defer fmt.Println("333333333333")
+	// defer 会将操作依次入栈 先进后出
+	defer fmt.Println("111 n1 =", n1)
+	defer fmt.Println("111 n2 =", n2)
 
-	fmt.Println("444444444444")
+	res := n1 + n2
+	fmt.Println("222 res =", res)
+	n1++
+	n2++
+	res = n1 + n2
+	fmt.Printf("333 n1=%d, n2=%d, res=%d \n", n1, n2, res)
 
-	return n1 + n2
+	return res
+}
+
+// defer 最佳实践
+func monitor() {
+	fmt.Println("1.1 加载驱动,建立数据库连接")
+	defer fmt.Println("1.2 关闭数据库连接")
+
+	fmt.Println("\t2.1 准备运行环境")
+	defer fmt.Println("\t2.2 关闭运行环境")
+
+	fmt.Println("\t\t3.1 SQL线程开始执行")
+	defer fmt.Println("\t\t3.2 关闭SQL线程")
+
+	fmt.Println("\t\t\t4.0 处理运行结果")
 }
